56f6a0e6dba06186ce8d3029156708cd8dd9bcc0
AlonsoRiva <al.rivadeneira@duocuc.cl>
2026-01-13 17:04:30 -0300

    Creacion de las operaciones crud y logica controller

--- PerfulandiaApiUsuarios/src/main/java/com/example/PerfulandiaApiUsuarios/controller/UsuarioController.java
+++ PerfulandiaApiUsuarios/src/main/java/com/example/PerfulandiaApiUsuarios/controller/UsuarioController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.PerfulandiaApiUsuarios.models.entities.Usuario;
+import com.example.PerfulandiaApiUsuarios.models.request.ActualizarUsuario;
+import com.example.PerfulandiaApiUsuarios.models.request.AgregarUsuario;
 import com.example.PerfulandiaApiUsuarios.services.UsuarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping
 @RestController
@@ -21,9 +27,30 @@ public class UsuarioController {
 
     @GetMapping("")
     public List<Usuario> obtenerTodo(){
-        return UsuarioService.ObtenerTodoslosUsuarios();
+        return usuarioService.ObtenerTodoslosUsuarios();
 
     }
     
+    @GetMapping("/{idUsuario}")
+    public Usuario getMethodName(@PathVariable int idUsuario) {
+        return usuarioService.obtenerUsuarioPorId(idUsuario);
+    }
+
+    @PostMapping("")
+    public Usuario AgregarUsuario(@RequestBody  AgregarUsuario nueva) {
+        
+        return usuarioService.agregarNuevoUsuario(nueva);
+    }
     
+    @PutMapping("")
+    public Usuario actualizMarca(@RequestBody ActualizarUsuario nueva) {
+        
+        return usuarioService.actualizarUsuario(nueva);
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public String eliminarMarca(@PathVariable int idMarca){
+        return usuarioService.eliminarUsuario(idMarca);
+
+    }
 }
