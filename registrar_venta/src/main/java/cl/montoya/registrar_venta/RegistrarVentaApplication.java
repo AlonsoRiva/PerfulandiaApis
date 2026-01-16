2ddb8910ab81e498a5f923796ea33225bbc9044a
NekoKen <dav.montoya@duocuc.cl>
2026-01-16 04:38:00 -0300

    genera ventas, funciona independiente

---
+++ registrar_venta/src/main/java/cl/montoya/registrar_venta/RegistrarVentaApplication.java
@@ -0,0 +1,13 @@
+package cl.montoya.registrar_venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarVentaApplication.class, args);
+	}
+
+}
---
+++ registrar_venta/src/main/java/cl/montoya/registrar_venta/models/request/AgregarVenta.java
@@ -0,0 +1,35 @@
+package cl.montoya.registrar_venta.models.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class AgregarVenta {
+
+    @NotBlank
+    private int id_usuario;
+
+    @NotBlank
+    private int id_producto;
+
+    @NotBlank
+    private String nombre_producto;
+
+    @NotBlank
+    private int cantidad_producto;
+
+    @NotBlank
+    private int id_cliente;
+    
+    @NotBlank
+    private String nombre_cliente;
+    
+    @NotBlank
+    private String metodo_pago;
+
+    @NotBlank
+    private String fecha_venta;
+
+    @NotBlank
+    private int monto_total;
+}
