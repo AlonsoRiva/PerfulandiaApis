3462108a92e83fde0f9420c494fb4ceebee4d0fc
NekoKen <dav.montoya@duocuc.cl>
2026-01-16 04:31:35 -0300

    para reclamos, funciona de forma independiente

---
+++ gestion_reclamo/src/main/java/cl/montoya/gestion_reclamo/GestionReclamoApplication.java
@@ -0,0 +1,13 @@
+package cl.montoya.gestion_reclamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReclamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionReclamoApplication.class, args);
+	}
+
+}
