1de6872f5d3b9984d46c06fcbaa4d0d8550825f2
Alonso Rivadeneira <al.rivadeneira@duocuc.cl>
2026-01-22 16:38:50 -0300

    Implementacion de swagger

---
+++ PerfulandiaApiMonitorizacion/src/main/java/com/example/PerfulandiaApiMonitorizacion/config/OpenApiConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.PerfulandiaApiMonitorizacion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI documentacionOpenApi(){
+        return new OpenAPI()
+            .info(new Info()
+            .title("Perfulandia - API Monitorización")
+            .version("1.0")
+            .description("Servicio para el seguimiento del estado de salud del sistema")
+            .contact(new Contact()
+                .name("Los Crocodilitos")));
+    }
+}
